5346538581b14287f43069b1243de425535699a0
Ole-Kristian <ole-kristian@rudberg.as>
2022-03-22 13:39:35 +0100

    Fjernet applikasjoner ettersom de ligger i eget repo: sonjoh/bachelor-applikasjon

--- applikasjoner/django-applikasjon/api/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class AppDB(models.Model):
-    tidspunkt = models.CharField(max_length=75, default="")
-    tid = models.CharField(max_length=75, default="")
-    intervall = models.CharField(max_length=75, default="")
-    tid_siden_siste = models.CharField(max_length=75, default="")
-    pod_navn = models.CharField(max_length=75, default="")
